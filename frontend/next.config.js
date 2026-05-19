987a34db5ee6ec894037db8df17388e56759be2b
latiblack <Lateefolalekan18@yahoo.com>
2026-05-19 18:31:01 +0000

    Move Next.js app to repo root so Vercel finds it automatically - fixes 404

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname),
-};
-
-module.exports = nextConfig;
